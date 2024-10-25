e9893db4208c7395fe76763dd8f8cb32f64f1cc5
Seth Underwood <Seth.Underwood@noaa.gov>
2024-10-25 10:07:16 -0400

    Add LLVM to path, if it exists

--- plugins/rdhpcs_env/profile.d/gfdl.noaa.gov/mlt/homebrew.sh
+++ plugins/rdhpcs_env/profile.d/gfdl.noaa.gov/mlt/homebrew.sh
@@ -7,3 +7,10 @@ fi
 # Other useful environment variables
 export HOMEBREW_CASK_OPTS="--appdir=/Users/Seth.Underwood/Applications"
 export HOMEBREW_NO_ANALYTICS=1
+
+# Add LLVM to path, if installed
+if [ -e "$HOMEBREW_PREFIX/opt/llvm/bin" ]
+then
+  pathmunge "$HOMEBREW_PREFIX/opt/llvm/bin" after
+fi
+
