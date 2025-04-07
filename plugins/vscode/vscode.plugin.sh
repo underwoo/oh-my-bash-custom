ce30ba9d8b01b5b4e3e4f971cdda0e79db143949
Seth Underwood <Seth.Underwood@noaa.gov>
2025-04-07 10:47:36 -0400

    fix MacOS code for vscode

--- plugins/vscode/vscode.plugin.sh
+++ plugins/vscode/vscode.plugin.sh
@@ -1,10 +1,14 @@
 # On MacOS, it is possible Visual Studio Code is not in /Applications
 # Use `osascript` to get the POSIX path to "Visual Studio Code.app"
 
-if [[ $OSTYPE == darwin* ]]
+if [[ x"$(command -v code)" == "x" ]]
 then
-    _code_path=$(osascript -e 'POSIX path of (path to application "Visual Studio Code")')
-    # Add Visual Studio Code (code) to the path
-    export PATH="${PATH}:${_code_path}/Contents/Resources/app/bin"
-    unset _code_path
+    # The code command was not found in PATH, add it
+    if [[ $OSTYPE == darwin* ]]
+    then
+        _code_path=$(osascript -e 'POSIX path of (path to application "Visual Studio Code")')
+        # Add Visual Studio Code (code) to the path
+        export PATH="${PATH}:${_code_path}/Contents/Resources/app/bin"
+        unset _code_path
+    fi
 fi
