f31e044266eccb2179a4293f8c04f8bbc8d5b1c1
Seth Underwood <Seth.Underwood@noaa.gov>
2025-07-30 14:18:18 -0400

    Add conda plugin

---
+++ plugins/conda/conda.plugin.sh
@@ -0,0 +1,16 @@
+__conda_base='/app/conda/miniforge'
+__conda_setup="$('$__conda_basebin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$__conda_base/etc/profile.d/conda.sh" ]; then
+        . "$__conda_base/etc/profile.d/conda.sh"
+    else
+        export PATH="$__conda_base/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+unset __conda_base
+
+## If desired, I can set aliases and other settings here.
+
